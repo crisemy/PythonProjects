75f07ec389764df1a9ae469869044895425ccb94
Cris Nadj <crisemy@gmail.com>
2019-02-06 18:00:28 -0300

    Search: Searching by Empty Values x 2

--- Pages/python_home_page.py
+++ Pages/python_home_page.py
@@ -13,4 +13,20 @@ class PythonHomePage(BasePage):
     @property
     def go_button(self):
         locator = (By.ID, 'submit') # I'm sending the locator to search for AND the value
-        return BaseElement(self.driver, by=locator[0], value=locator[1]) # Returns the by, driver and value
+        return BaseElement(self.driver, by=locator[0], value=locator[1])
+
+    # #03: Search: Searching by Empty Values
+
+    # Clicking on the Logo to start searching right from the Home Page
+    @property
+    def go_logo_image(self):
+        locator = (By.CLASS_NAME, "python-logo")
+        return BaseElement(self.driver, by=locator[0], value=locator[1])
+
+    # Compare whether the Title exists or not
+    @property
+    def assert_title(self):
+        # Searches by Title - H2 in this case
+
+        locator = (By.CSS_SELECTOR, 'h2')
+        return BaseElement(self.driver, by=locator[0], value=locator[1])
